9e26a0a445237d0496691757957566fc46a9c3e7
Suraj Mishra <mi.suraj8833@gmail.com>
2021-11-28 20:55:32 +0530

    Cleaning up the code.

--- src/main/java/uk/tw/energy/service/MeterReadingService.java
+++ src/main/java/uk/tw/energy/service/MeterReadingService.java
@@ -1,7 +1,5 @@
 package uk.tw.energy.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import uk.tw.energy.domain.ElectricityReading;
 
